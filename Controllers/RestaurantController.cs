7fc3fcdcf09f471207bda8af2c303ea090fa2d9d
Brianne W <brianne4god@gmail.com>
2023-07-03 13:50:34 -0400

    service constructor

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using RestaurantRaterMVC.Services.Restaurant;
+
+namespace RestaurantRaterMVC.Controllers;
+
+public class RestaurantController : Controller
+{
+    private IRestaurantService _service;
+    public RestaurantController(IRestaurantService service)
+    {
+        _service = service;
+    }
+}
